3a8d8b51b2b86fabd6912a902ef044b340902303
Hector <hector.quijada2121@gmail.com>
2022-02-03 16:26:23 -0500

    frontend setup

---
+++ frontend/target/wasm32-unknown-unknown/debug/deps/frontend-fb1bd9d22864c11f.d
@@ -0,0 +1,5 @@
+/Users/hq/Desktop/techno/rust-pro/tauri-yew-desktop/frontend/target/wasm32-unknown-unknown/debug/deps/frontend-fb1bd9d22864c11f.wasm: src/main.rs
+
+/Users/hq/Desktop/techno/rust-pro/tauri-yew-desktop/frontend/target/wasm32-unknown-unknown/debug/deps/frontend-fb1bd9d22864c11f.d: src/main.rs
+
+src/main.rs:
---
+++ frontend/target/wasm32-unknown-unknown/debug/frontend.d
@@ -0,0 +1,1 @@
+/Users/hq/Desktop/techno/rust-pro/tauri-yew-desktop/frontend/target/wasm32-unknown-unknown/debug/frontend.wasm: /Users/hq/Desktop/techno/rust-pro/tauri-yew-desktop/frontend/src/main.rs
